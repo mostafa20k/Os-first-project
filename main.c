cfe5869d1a79448ed30993aa8184a8aaebe1e3c6
Hesam760 <ghasemi.hesam3@gmail.com>
2022-12-22 10:08:21 +0330

    add functions for string

--- main.c
+++ main.c
@@ -1,6 +1,3 @@
-//
-// Created by mostafa on 15.12.22.
-//
 // C Program to design a shell in Linux
 #include<stdio.h>
 #include<string.h>
@@ -18,8 +15,7 @@
 #define clear() printf("\033[H\033[J")
 
 // Greeting shell during startup
-void init_shell()
-{
+void init_shell() {
     clear();
     printf("\n\n\n\n******************"
            "************************");
@@ -27,7 +23,7 @@ void init_shell()
     printf("\n\n\t-USE AT YOUR OWN RISK-");
     printf("\n\n\n\n*******************"
            "***********************");
-    char* username = getenv("USER");
+    char *username = getenv("USER");
     printf("\n\n\nUSER is: @%s", username);
     printf("\n");
     sleep(1);
@@ -35,9 +31,8 @@ void init_shell()
 }
 
 // Function to take input
-int takeInput(char* str)
-{
-    char* buf;
+int takeInput(char *str) {
+    char *buf;
 
     buf = readline("\n>>> ");
     if (strlen(buf) != 0) {
@@ -50,16 +45,14 @@ int takeInput(char* str)
 }
 
 // Function to print Current Directory.
-void printDir()
-{
+void printDir() {
     char cwd[1024];
     getcwd(cwd, sizeof(cwd));
     printf("\nDir: %s", cwd);
 }
 
 // Function where the system command is executed
-void execArgs(char** parsed)
-{
+void execArgs(char **parsed) {
     // Forking a child
     pid_t pid = fork();
 
@@ -79,8 +72,7 @@ void execArgs(char** parsed)
 }
 
 // Function where the piped system commands is executed
-void execArgsPiped(char** parsed, char** parsedpipe)
-{
+void execArgsPiped(char **parsed, char **parsedpipe) {
     // 0 is read end, 1 is write end
     int pipefd[2];
     pid_t p1, p2;
@@ -134,14 +126,19 @@ void execArgsPiped(char** parsed, char** parsedpipe)
 }
 
 // Help command builtin
-void openHelp()
-{
+void openHelp() {
     puts("\n***WELCOME TO MY SHELL HELP***"
          "\nCopyright @ Suprotik Dey"
          "\n-Use the shell at your own risk..."
          "\nList of Commands supported:"
          "\n>cd"
          "\n>ls"
+         "\nfirststr"
+         "\nmostreap"
+         "\nnospace"
+         "\nnotcomment"
+         "\nnumline"
+         "\ntenline"
          "\n>exit"
          "\n>all other general commands available in UNIX shell"
          "\n>pipe handling"
@@ -151,16 +148,22 @@ void openHelp()
 }
 
 // Function to execute builtin commands
-int ownCmdHandler(char** parsed)
-{
-    int NoOfOwnCmds = 4, i, switchOwnArg = 0;
-    char* ListOfOwnCmds[NoOfOwnCmds];
-    char* username;
+int ownCmdHandler(char **parsed) {
+    int NoOfOwnCmds = 10, i, switchOwnArg = 0;
+    char *ListOfOwnCmds[NoOfOwnCmds];
+    char *username;
 
     ListOfOwnCmds[0] = "exit";
     ListOfOwnCmds[1] = "cd";
     ListOfOwnCmds[2] = "help";
     ListOfOwnCmds[3] = "hello";
+    ListOfOwnCmds[4] = "firststr";
+    ListOfOwnCmds[5] = "nospace";
+    ListOfOwnCmds[6] = "mostrep";
+    ListOfOwnCmds[7] = "numline";
+    ListOfOwnCmds[8] = "tenline";
+    ListOfOwnCmds[9] = "nocomment";
+
 
     for (i = 0; i < NoOfOwnCmds; i++) {
         if (strcmp(parsed[0], ListOfOwnCmds[i]) == 0) {
@@ -186,6 +189,52 @@ int ownCmdHandler(char** parsed)
                    "\nUse help to know more..\n",
                    username);
             return 1;
+        case 5:
+            if (parsed[1] != NULL) {
+                firstStr(parsed[1]);
+            } else {
+                printf("No File Detected !");
+            }
+            return 1;
+        case 6:
+            if (parsed[1] != NULL) {
+                emptySpace(parsed[1]);
+            } else {
+                printf("No File Detected !");
+            }
+            return 1;
+        case 7:
+            if (parsed[1] != NULL){
+                mostRepeated(parsed[1]);
+            }
+            else {
+                printf("No File Detected !");
+            }
+            return 1;
+        case 8:
+            if (parsed[1] != NULL){
+                numLine(parsed[1]);
+            }
+            else {
+                printf("No File Detected !");
+            }
+            return 1;
+        case 9:
+            if (parsed[1] != NULL){
+                tenLine(parsed[1]);
+            }
+            else {
+                printf("No File Detected !");
+            }
+            return 1;
+        case 10:
+            if (parsed[1] != NULL){
+                noComment(parsed[1]);
+            }
+            else {
+                printf("No File Detected !");
+            }
+            return 1;
         default:
             break;
     }
@@ -193,9 +242,147 @@ int ownCmdHandler(char** parsed)
     return 0;
 }
 
+void firstStr(char *ch) {
+    FILE *fPointer, *fWords, *fWordCopy;
+    char singleLine[150];
+    if (access(ch, F_OK) == 0) {
+        fPointer = fopen(ch, "r");
+
+        char *pch;
+        while (!feof(fPointer)) {
+            fgets(singleLine, 100, fPointer);
+            pch = strtok(singleLine, " ,'(");
+            printf("%s\n", pch);
+        }
+        fclose(fPointer);
+    } else {
+        printf("File Not Found !");
+    }
+}
+
+void emptySpace(char *ch) {
+    FILE *filePtr;
+    char fileBuff[255];
+    int fileChar;
+
+    filePtr = fopen(ch, "r");
+    if (filePtr == NULL) {
+        printf("file not found");
+        return;
+    }
+
+    fgets(fileBuff, 255, filePtr);
+    fseek(filePtr, 0, SEEK_SET);
+    do {
+        fileChar = fgetc(filePtr);
+        if (isgraph(fileChar)) {
+            putchar(fileChar);
+        }
+    } while (fileChar != EOF);
+    fclose(filePtr);
+    return;
+}
+
+void mostRepeated(char *ch1) {
+    FILE *file;
+    char ch, *line;
+    size_t len = 0, read;
+    char words[1000][1000], word[20];
+    int i = 0, j = 0, k = 0, maxCount = 0, count = 0;
+
+    file = fopen(ch1, "r");
+    if (file == NULL) {
+        printf("File Not Found !");
+    }
+    while ((read = getline(&line, &len, file)) != -1) {
+        for (k = 0; line[k] != '\0'; k++) {
+            if (line[k] != ' ' && line[k] != '\n' && line[k] != ',' && line[k] != '.') {
+                words[i][j++] = tolower(line[k]);
+            } else {
+                words[i][j] = '\0';
+                i++;
+                j = 0;
+            }
+        }
+    }
+    int length = i;
+    for (int i = 0; i < length; i++){
+        count = 1;
+        for(j = i + 1; j < length; j++){
+            if (strcmp(words[i],words[j]) == 0 && (strcmp(words[i],"")!= 0)){
+                count++;
+            }
+        }
+        if (count > maxCount){
+            maxCount = count;
+            strcpy(word, words[i]);
+        }
+    }
+    printf("Most Repeated word is : %s " , word);
+    fclose(file);
+    return;
+}
+
+void numLine(char *ch){
+    FILE *fPointer;
+    int count = 0;
+    char c;
+    if (access(ch, F_OK) == 0) {
+        fPointer = fopen(ch, "r");
+        for(c = getc(fPointer); c != EOF; c = getc(fPointer)){
+            if (c == '\n')
+                count++;
+        }
+        printf("Number of Line is : %d" , count + 1);
+        fclose(fPointer);
+    } else {
+        printf("File Not Found !");
+    }
+}
+
+void tenLine (char *ch) {
+    FILE *fPointer, *fWords, *fWordCopy;
+    char singleLine[150];
+    int count = 0;
+    if (access(ch, F_OK) == 0) {
+        fPointer = fopen(ch, "r");
+
+        char *pch;
+        while (!feof(fPointer) && count < 10) {
+            fgets(singleLine, 100, fPointer);
+            printf("%s" ,singleLine);
+            count++;
+        }
+        fclose(fPointer);
+    } else {
+        printf("File Not Found !");
+    }
+}
+
+void noComment (char *ch){
+    FILE *fPointer, *fWords, *fWordCopy;
+    char singleLine[150];
+    if (access(ch, F_OK) == 0) {
+        fPointer = fopen(ch, "r");
+
+        char *pch;
+        while (!feof(fPointer)) {
+            fgets(singleLine, 100, fPointer);
+            pch = strtok(singleLine, " ,'(");
+            if (pch[0] == '#') {
+                continue;
+            }
+            else {
+                printf("%s", singleLine);
+            }
+        }
+        fclose(fPointer);
+    } else {
+        printf("File Not Found !");
+    }
+}
 // function for finding pipe
-int parsePipe(char* str, char** strpiped)
-{
+int parsePipe(char *str, char **strpiped) {
     int i;
     for (i = 0; i < 2; i++) {
         strpiped[i] = strsep(&str, "|");
@@ -211,8 +398,7 @@ int parsePipe(char* str, char** strpiped)
 }
 
 // function for parsing command words
-void parseSpace(char* str, char** parsed)
-{
+void parseSpace(char *str, char **parsed) {
     int i;
 
     for (i = 0; i < MAXLIST; i++) {
@@ -225,10 +411,9 @@ void parseSpace(char* str, char** parsed)
     }
 }
 
-int processString(char* str, char** parsed, char** parsedpipe)
-{
+int processString(char *str, char **parsed, char **parsedpipe) {
 
-    char* strpiped[2];
+    char *strpiped[2];
     int piped = 0;
 
     piped = parsePipe(str, strpiped);
@@ -248,13 +433,10 @@ int processString(char* str, char** parsed, char** parsedpipe)
         return 1 + piped;
 }
 
-int main()
-{
+int main() {
     char inputString[MAXCOM], *parsedArgs[MAXLIST];
-    char* parsedArgsPiped[MAXLIST];
-    // if the command need to be piped
+    char *parsedArgsPiped[MAXLIST];
     int execFlag = 0;
-    //print shell default
     init_shell();
 
     while (1) {
@@ -270,6 +452,7 @@ int main()
         // or it is a builtin command,
         // 1 if it is a simple command
         // 2 if it is including a pipe.
+//        printf("%d", execFlag);
 
         // execute
         if (execFlag == 1)
@@ -280,5 +463,3 @@ int main()
     }
     return 0;
 }
-
-
